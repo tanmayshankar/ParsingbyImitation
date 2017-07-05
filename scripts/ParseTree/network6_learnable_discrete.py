e8801f48dc5235193874447799af1d741f2e6065
tanmayshankar <tanmay.shankar@gmail.com>
2017-07-05 11:46:17 -0400

    Adding epislon noise to categorical probability.

--- scripts/ParseTree/network6_learnable_discrete.py
+++ scripts/ParseTree/network6_learnable_discrete.py
@@ -220,13 +220,26 @@ class hierarchical():
 
 				# REMEMBER, h is along y, w is along x (transposed), # FOR THESE RULES, use y_gradient
 				while (split_location<=0)or(split_location>=self.state.h):				
-					# print(split_location,self.state.h)
+
 					categorical_prob_softmax = self.sess.run(self.categorical_probabilities, feed_dict={self.gradient_values: self.y_gradients.reshape((1,20))})[0]
-					categorical_prob_softmax[[0,-1]]=0.
+					epsilon = 0.001
+					categorical_prob_softmax+=epsilon
+					categorical_prob_softmax[0] = 0.
+					categorical_prob_softmax[-1] = 0.
 					categorical_prob_softmax /= categorical_prob_softmax.sum()
 					split_location = npy.random.choice(range(20),p=categorical_prob_softmax)
 					split_location = int(float(self.state.h*split_location)/20)				
 					counter +=1
+					if counter>=25:
+						print("PREINT:",split_location,self.state.h)
+
+					if split_location>=10:
+						split_location = int(npy.floor(float(self.state.h*split_location)/20))
+					else:
+						split_location = int(npy.ceil(float(self.state.h*split_location)/20))
+					
+					if counter>=25:
+						print("POSTINT:",split_location,self.state.h)
 					
 				# Create splits.
 				s1 = parse_tree_node(label=indices[0],x=self.state.x,y=self.state.y,w=self.state.w,h=split_location,backward_index=self.current_parsing_index)
@@ -237,13 +250,25 @@ class hierarchical():
 
 				# REMEMBER, h is along y, w is along x (transposed), # FOR THESE RULES, use x_gradient
 				while (split_location<=0)or(split_location>=self.state.w):				
-					# print(split_location,self.state.w)
+
 					categorical_prob_softmax = self.sess.run(self.categorical_probabilities, feed_dict={self.gradient_values: self.x_gradients.reshape((1,20))})[0]
-					categorical_prob_softmax[[0,-1]]=0.
+					epsilon = 0.001
+					categorical_prob_softmax+=epsilon
+					categorical_prob_softmax[0] = 0.
+					categorical_prob_softmax[-1] = 0.
 					categorical_prob_softmax /= categorical_prob_softmax.sum()
-					split_location = npy.random.choice(range(20),p=categorical_prob_softmax)
-					split_location = int(float(self.state.w*split_location)/20)
+					split_location = npy.random.choice(range(20),p=categorical_prob_softmax)				
 					counter +=1
+					if counter>=25:
+						print("PREINT:",split_location,self.state.w)
+
+					if split_location>=10:
+						split_location = int(npy.floor(float(self.state.w*split_location)/20))
+					else:
+						split_location = int(npy.ceil(float(self.state.w*split_location)/20))
+
+					if counter>=25:
+						print("POSTINT:",split_location,self.state.w)
 
 				
 				# Create splits.
