fda42cc9b8a97b8b769b696fb855123b95013c46
tanmayshankar <tanmay.shankar@gmail.com>
2017-08-31 16:58:58 -0400

    Update

--- scripts/DeterministicGoals/net6_newpaint_continuity_large.py
+++ scripts/DeterministicGoals/net6_newpaint_continuity_large.py
@@ -718,7 +718,7 @@ class hierarchical():
 def main(args):
 
 	# # Create a TensorFlow session with limits on GPU usage.
-	gpu_ops = tf.GPUOptions(allow_growth=True,visible_device_list="0,3")
+	gpu_ops = tf.GPUOptions(allow_growth=True,visible_device_list="3,0")
 	config = tf.ConfigProto(gpu_options=gpu_ops)
 	sess = tf.Session(config=config)
 
