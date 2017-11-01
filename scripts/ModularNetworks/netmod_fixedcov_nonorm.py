63bba0864f5a4b8d62b92b881bc485bde8c4a5c7
tanmayshankar <tanmay.shankar@gmail.com>
2017-11-01 16:25:26 -0400

    Updating alpha to 1.1.

--- scripts/ModularNetworks/netmod_fixedcov_nonorm.py
+++ scripts/ModularNetworks/netmod_fixedcov_nonorm.py
@@ -562,7 +562,7 @@ class hierarchical():
 
         for j in range(len(self.parse_tree)):
             self.parse_tree[j].reward /= (self.parse_tree[j].w*self.parse_tree[j].h)            
-        self.alpha = 1.0
+        self.alpha = 1.1
         
         # Non-linearizing rewards.
         for j in range(len(self.parse_tree)):
