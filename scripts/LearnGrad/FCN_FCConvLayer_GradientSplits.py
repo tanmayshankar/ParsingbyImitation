7ec2ecdb568a42625dbb02c6d588612563ec0d59
tanmayshankar <tanmay.shankar@gmail.com>
2017-11-06 22:51:51 -0500

    Update

--- scripts/LearnGrad/FCN_FCConvLayer_GradientSplits.py
+++ scripts/LearnGrad/FCN_FCConvLayer_GradientSplits.py
@@ -722,8 +722,8 @@ class hierarchical():
 				# SAMPLING SPLIT LOCATION INSIDE THIS CONDITION:
 
 				while (split_location<=0)or(split_location>=self.state.h):
-					probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image.reshape(1,self.image_size,self.image_size,3), self.split_indicator: self.state.split_indicator})	
-
+					# probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image.reshape(1,self.image_size,self.image_size,3), self.split_indicator: self.state.split_indicator})	
+					probs = copy.deepcopy(self.horizontal_grad)
 					categorical_prob_softmax = copy.deepcopy(probs[0])
 					categorical_prob_softmax[[0,-1]] = 0.
 					categorical_prob_softmax = categorical_prob_softmax/categorical_prob_softmax.sum()
@@ -747,8 +747,8 @@ class hierarchical():
 				self.state.split_indicator = 1
 
 				while (split_location<=0)or(split_location>=self.state.w):
-					probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image.reshape(1,self.image_size,self.image_size,3), self.split_indicator: self.state.split_indicator})	
-
+					# probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image.reshape(1,self.image_size,self.image_size,3), self.split_indicator: self.state.split_indicator})	
+					probs = copy.deepcopy(self.vertical_grad)	
 					categorical_prob_softmax = copy.deepcopy(probs[0])
 					categorical_prob_softmax[[0,-1]] = 0.
 					categorical_prob_softmax = categorical_prob_softmax/categorical_prob_softmax.sum()
--- scripts/LearnGrad/FCN_globalspatialpooling_GradientSplits.py
+++ scripts/LearnGrad/FCN_globalspatialpooling_GradientSplits.py
@@ -733,8 +733,8 @@ class hierarchical():
 
 				while (split_location<=0)or(split_location>=self.state.h):
 					# probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image.reshape(1,self.image_size,self.image_size,3), self.split_indicator: self.state.split_indicator})	
-					probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image, self.split_indicator: self.state.split_indicator})	
-
+					# probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image, self.split_indicator: self.state.split_indicator})	
+					probs = copy.deepcopy(self.horizontal_grad)
 					epsilon = 0.0001
 					categorical_prob_softmax = copy.deepcopy(probs[0])
 					# categorical_prob_softmax += epsilon
@@ -762,7 +762,8 @@ class hierarchical():
 
 				while (split_location<=0)or(split_location>=self.state.w):
 					# probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image.reshape(1,self.image_size,self.image_size,3), self.split_indicator: self.state.split_indicator})	
-					probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image, self.split_indicator: self.state.split_indicator})	
+					# probs = self.sess.run(self.horizontal_grad, feed_dict={self.input: self.resized_image, self.split_indicator: self.state.split_indicator})	
+					probs = copy.deepcopy(self.vertical_grad)
 
 					epsilon = 0.0001
 					categorical_prob_softmax = copy.deepcopy(probs[0])
