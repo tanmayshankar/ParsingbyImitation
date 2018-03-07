be8012037d86704131c18eb678684bef6c9653c0
tanmayshankar <tanmay.shankar@gmail.com>
2018-03-07 13:34:35 -0500

    Update

--- OrganizedCode/Parsing/Meta_RLClass.py
+++ OrganizedCode/Parsing/Meta_RLClass.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from headers import *
-# import TF_Model_RuleSplit
 import TF_Model_RuleSplit_FixedCov
 import Data_Loader
-import NewParsing
+import Parsing
+import Memory
 
 class Meta_RLClass():
 
@@ -25,13 +25,6 @@ class Meta_RLClass():
 		self.data_loader.preprocess()
 		
 		# # Instantiate Model Class.
-		# if self.args.anneal_cov:			
-		# 	self.model = TF_Model_AnnealedCov.Model(num_channels=self.data_loader.num_channels)
-		# elif self.args.learn_cov:
-		# 	self.model = TF_Model_LearntCov.Model(num_channels=self.data_loader.num_channels)
-		# else:
-		# 	self.model = TF_Model_Class.Model(num_channels=self.data_loader.num_channels)
-		# self.model = TF_Model_RuleSplit.Model(num_channels=self.data_loader.num_channels)
 		self.model = TF_Model_RuleSplit_FixedCov.Model(num_channels=self.data_loader.num_channels)
 
 		if self.args.model:
@@ -39,24 +32,23 @@ class Meta_RLClass():
 		else:
 			self.model.create_network(self.sess,to_train=self.args.train)
 
+		# Instantiate memory. 
+		self.memory = Memory.Replay_Memory()
+
 		# self.plot_manager = Plotting_Utilities.PlotManager(to_plot=self.args.plot,parser=self.parser)
 
-		self.parser = NewParsing.Parser(self.model,self.data_loader,self.args,self.sess)
+		self.parser = Parsing.Parser(self.model,self.data_loader,self.memory,self.args,self.sess)
 
 	def train(self):
-		self.parser.meta_training()
+		self.parser.meta_training(self.args.train)
 
 def parse_arguments():
 	parser = argparse.ArgumentParser(description='Primitive-Aware Segmentation Argument Parsing')
 	parser.add_argument('--images',dest='images',type=str)
 	parser.add_argument('--labels',dest='labels',type=str)
 	parser.add_argument('--indices',dest='indices',type=str)
-	parser.add_argument('--horrew',dest='horrew',type=str)
-	parser.add_argument('--learncov',dest='learn_cov',type=int,default=0)
-	parser.add_argument('--annealcov',dest='anneal_cov',type=int,default=0)
 	parser.add_argument('--suffix',dest='suffix',type=str)
 	parser.add_argument('--gpu',dest='gpu')
-	parser.add_argument('--plot',dest='plot',type=int,default=0)
 	parser.add_argument('--train',dest='train',type=int,default=1)
 	parser.add_argument('--model',dest='model',type=str)
 	return parser.parse_args()
@@ -64,7 +56,6 @@ def parse_arguments():
 def main(args):
 
 	args = parse_arguments()
-	print(args)
 
 	# # Create a TensorFlow session with limits on GPU usage.
 	gpu_ops = tf.GPUOptions(allow_growth=True,visible_device_list=args.gpu)
@@ -73,17 +64,6 @@ def main(args):
 	sess = tf.Session(config=config)
 
 	hierarchical_model = Meta_RLClass(session=sess,arguments=args)
-
-	print("Loading Images.")
-	hierarchical_model.images = npy.load(args.images)	
-	hierarchical_model.true_labels = npy.load(args.labels) 
-
-	hierarchical_model.plot = args.plot
-	hierarchical_model.to_train = args.train
-	
-	if hierarchical_model.to_train:
-		hierarchical_model.suffix = args.suffix
-	print("Starting to Train.")
 
 	hierarchical_model.train()
 
