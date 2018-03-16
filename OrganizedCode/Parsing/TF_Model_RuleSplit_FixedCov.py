7152522d64e7e0c0078d16ad3dea26c146f5ce64
tanmayshankar <tanmay.shankar@gmail.com>
2018-03-16 13:19:17 -0400

    update

--- OrganizedCode/Parsing/TF_Model_RuleSplit_FixedCov.py
+++ OrganizedCode/Parsing/TF_Model_RuleSplit_FixedCov.py
@@ -12,7 +12,7 @@ class Model():
 		# Initializing the session.
 		self.sess = sess
 		self.to_train = to_train
-		print("TO TRAIN:",self.to_train)
+
 		# Number of layers. 
 		self.num_layers = 5
 		self.num_fc_layers = 2
@@ -68,10 +68,8 @@ class Model():
 		self.split_mean = tf.layers.dense(self.fc6,1,activation=tf.nn.sigmoid)
 
 		if self.to_train:
-			print("To train was true")
 			self.split_cov = 0.05
 		else:
-			print("To train was not true")
 			self.split_cov = 0.001
 
 		self.split_dist = tf.contrib.distributions.Normal(loc=self.split_mean,scale=self.split_cov)
@@ -131,8 +129,6 @@ class Model():
 
 	def create_network(self, sess, pretrained_weight_file=None, to_train=False):
 
-		print("Training Policy from base model.")
-		print(to_train)
 		self.initialize_base_model(sess,to_train=to_train)
 		self.define_rule_stream()
 		self.define_split_stream()
