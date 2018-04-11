068125a641e0d45b625d81008a6c31aa9b8d200d
tanmayshankar <tanmay.shankar@gmail.com>
2018-04-11 01:22:21 -0400

    Adding Paintbrush width restricted code.

---
+++ OrganizedCode/OffPolicyPainting/Meta_TestScript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from headers import *
+
+for i in range(0,260,10):
+# for i in range(10,260,10):
+	print("###############################################")
+	print("STARTING WITH:",i)
+	# FOR FULL PARSING:
+	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350BINLABS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --train 0 --gpu 0,1 --depth_terminate 1 --model saved_models/model_epoch{0}.ckpt --suffix model_dt4_{0}".format(i)
+	subprocess.call(command.split(),shell=False)
