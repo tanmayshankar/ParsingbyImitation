7b78ddf1dd6f7f6b5a24e9e3e9283d98262c964c
tanmayshankar <tanmay.shankar@gmail.com>
2018-04-24 14:59:22 -0400

    Adding logit normal distribution stuff.

--- OrganizedCode/Imitation/Meta_RLClass.py
+++ OrganizedCode/Imitation/Meta_RLClass.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 from headers import *
 import TF_Model
-import BetaDist_TFModel
+# import BetaDist_TFModel
+import LogitNormal_TFModel
+import LogitNormal_Parser
 import Data_Loader
 import DaggerImitationParser
 import NewPlotting
@@ -25,8 +27,8 @@ class Meta_RLClass():
 		self.data_loader.preprocess()
 		
 		# # Instantiate Model Class.		
-		if self.args.beta:
-			self.model = BetaDist_TFModel.Model(num_channels=self.data_loader.num_channels)
+		if self.args.logit:
+			self.model = LogitNormal_TFModel.Model(num_channels=self.data_loader.num_channels)
 		else:
 			self.model = TF_Model.Model(num_channels=self.data_loader.num_channels)		
 		self.args.train = bool(self.args.train)
@@ -40,9 +42,13 @@ class Meta_RLClass():
 
 		# Instantiate the plotting manager. 
 		self.plotting_manager = NewPlotting.PlotManager(to_plot=self.args.plot,data_loader=self.data_loader)		
-
+	
 		# Instantiate parser, passing arguments to take care of train / test / IGM within the parsing code. 
-		self.parser = DaggerImitationParser.Parser(model_instance=self.model, data_loader_instance=self.data_loader,
+		if self.args.logit:
+			self.parser = LogitNormalParser.Parser(model_instance=self.model, data_loader_instance=self.data_loader,
+				memory_instance=self.memory, plot_manager=self.plotting_manager, args=self.args, session=self.sess)
+		else:
+			self.parser = DaggerImitationParser.Parser(model_instance=self.model, data_loader_instance=self.data_loader,
 				memory_instance=self.memory, plot_manager=self.plotting_manager, args=self.args, session=self.sess)
 
 	def train(self):
@@ -56,7 +62,8 @@ def parse_arguments():
 	parser.add_argument('--suffix',dest='suffix',type=str)
 	parser.add_argument('--plot',dest='plot',type=int,default=0)
 	parser.add_argument('--gpu',dest='gpu')
-	parser.add_argument('--beta',dest='beta',type=int,default=0)
+	# parser.add_argument('--beta',dest='beta',type=int,default=0)
+	parser.add_argument('--logit',dest='logit',type=int,default=0)
 	parser.add_argument('--train',dest='train',type=int,default=1)
 	parser.add_argument('--depth_terminate',dest='depth_terminate',type=int,default=0)
 	parser.add_argument('--model',dest='model',type=str)
