086dd9eab8fc6d868c5eed2f10030166c58fc1cf
tanmayshankar <tanmay.shankar@gmail.com>
2018-05-23 12:22:40 -0400

    Update

---
+++ LSTMParsing/LSTM_Trial1/Memory.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from headers import *
+
+class Replay_Memory():
+
+	def __init__(self, memory_size=2000):
+
+		self.memory = []
+		
+		self.memory_size = memory_size
+
+		# Essentially stores everything we need for backprop.
+		# Inputs --> images, for which we need state and boundaries.
+		# Sampled split. 
+		# Rule applied (target rule).
+		# Split weight (return).
+		# Rule weight.
+
+	def append_to_memory(self, state, k, parse_tree):
+		# The neat thing is now the "State" class is sufficient - no need to explicitly provide
+		# transitions as is typical of a memory. State class members have rule / split / masks. 
+
+		memory_len = len(self.memory)
+
+		if memory_len<self.memory_size:
+			self.memory.append([state,k,parse_tree])
+		else:
+			self.memory.pop(0)
+			self.memory.append([state,k,parse_tree])
+
+	def sample_batch(self, batch_size=10):
+		
+		memory_len = len(self.memory)	
+		indices = npy.random.randint(0,high=memory_len,size=(batch_size))
+		# indices = range(0,batch_size)
+
+		# for k in range(batch_size):
+		# 	self.memory.pop(k)
+
+		# The data loader is really what is storing the images. 
+		# Instead of creating a batch of data (such as image inputs etc.) here,
+		# pass indices back to parsing code. 
+		# Do the actual batching there, so that we don't have to pass data_loader here. 
+		# (Because transitions, i.e. state etc. only have indices / coordinates, not the actual image values.)
+		return indices
---
+++ LSTMParsing/LSTM_Trial1/TFModel.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+from headers import *
+
+class Model():
+
+	def __init__(self, image_size=256, num_channels=1):
+		self.image_size = image_size
+		self.num_channels = num_channels
+		self.num_layers = 7
+
+	def initialize_base_model(self, sess, model_file=None, to_train=None):
+
+		# Initializing the session.
+		self.sess = sess
+		self.to_train = to_train
+
+		# Number of layers. 
+		self.num_fc_layers = 2
+		self.conv_sizes = 3*npy.ones((self.num_layers),dtype=int)		
+		self.conv_num_filters = 20*npy.ones((self.num_layers),dtype=int)
+		self.conv_strides = 2*npy.ones((self.num_layers),dtype=int)
+
+		self.conv_strides[0:3] = 1
+		# # Strides are now: 1,1,1,2,2,2,2
+
+		# Placeholders
+		# Now doing this for single channel images.
+		self.input = tf.placeholder(tf.float32,shape=[None,self.image_size,self.image_size,self.num_channels],name='input')
+
+		self.max_timesteps = 24
+		# self.input = tf.placeholder(tf.float32,shape=[None,self.max_timesteps,self.image_size, self.image_size, self.num_channels],name='input')
+
+		# Reshaping input to (BATCHxTIME) x W x H x (Num channels=1)
+		# self.reshaped_input = tf.reshape(self.input, (-1,self.image_size,self.image_size,self.num_channels))
+
+		# Defining conv layers.
+		self.conv = [[] for i in range(self.num_layers)]
+
+		# Initial layer.
+		self.conv[0] = tf.layers.conv2d(self.input,filters=self.conv_num_filters[0],kernel_size=(self.conv_sizes[0]),strides=(self.conv_strides[0]),activation=tf.nn.relu,name='conv0')
+
+		# Defining subsequent conv layers.
+		for i in range(1,self.num_layers):
+			self.conv[i] = tf.layers.conv2d(self.conv[i-1],filters=self.conv_num_filters[i],kernel_size=(self.conv_sizes[i]),strides=(self.conv_strides[i]),activation=tf.nn.relu,name='conv{0}'.format(i))
+
+		self.final_conv_shape = self.conv[-1].get_shape().as_list()
+
+		# Instead of the flatten, reshape back to BATCH x TIME x D
+		# Here, D should be number of conv filters in the last
+		self.reshape_conv = tf.reshape(self.conv[-1],(-1,self.max_timesteps,self.conv_num_filters[-1]*self.final_conv_shape[-2]*self.final_conv_shape[-3]))
+		# embed()
+			
+	def define_LSTM(self):
+
+		self.fc6_shape = 1000
+		self.fc7_shape = 200
+		self.fc6 = tf.layers.dense(self.reshape_conv,self.fc6_shape,activation=tf.nn.relu)
+		self.fc7 = tf.layers.dense(self.fc6,self.fc7_shape,activation=tf.nn.relu)
+
+		# Now defining LSTM that takes fc7 as input.
+		self.lstm_numunits = 100
+		self.cell = tf.nn.rnn_cell.LSTMCell(num_units=self.lstm_numunits,state_is_tuple=True)
+
+		# Placeholder for actual sequence lengths.
+		self.sequence_length = tf.placeholder(tf.int32,shape=(None,),name='sequence_length')
+
+		# Defining the unrolled graph.
+		self.outputs, self.states = tf.nn.dynamic_rnn(cell=self.cell,dtype=tf.float32,sequence_length=self.sequence_length,inputs=self.fc7)
+	
+		self.lstm_hidden_state = self.states.h		
+
+	def define_rule_stream(self):
+
+		self.rule_fc8_shape = 100		
+		self.rule_fc8 = tf.layers.dense(self.lstm_hidden_state,self.rule_fc8_shape,activation=tf.nn.relu)
+
+		self.num_rules = 4
+		self.rule_presoftmax = tf.layers.dense(self.rule_fc8,self.num_rules)
+		self.rule_mask = tf.placeholder(tf.float32,shape=(None,self.num_rules))
+
+		self.softmax_numerator = tf.multiply(self.rule_mask,tf.exp(self.rule_presoftmax),name='softmax_numerator')
+		self.softmax_denominator = tf.add(tf.reduce_sum(tf.exp(tf.multiply(self.rule_mask,self.rule_presoftmax)),axis=-1,keep_dims=True),
+			tf.subtract(tf.reduce_sum(self.rule_mask,axis=-1,keep_dims=True),tf.constant(float(self.num_rules))))
+		
+		self.rule_probabilities = tf.divide(self.softmax_numerator,self.softmax_denominator)
+
+		self.rule_return_weight = tf.placeholder(tf.float32,shape=(None,1),name='rule_return_weight')
+		self.target_rule = tf.placeholder(tf.float32,shape=(None,self.num_rules),name='target_rule')
+
+		# self.target_rule = tf.placeholder(tf.float32,shape=(None,self.num_rules),name='target_rule')
+		self.rule_cross_entropy = tf.keras.backend.categorical_crossentropy(self.target_rule,self.rule_probabilities)
+		# self.rule_loss = tf.multiply(self.rule_return_weight,self.rule_cross_entropy)
+		self.rule_loss =  tf.multiply(self.rule_return_weight,tf.expand_dims(self.rule_cross_entropy,axis=-1))
+
+	def define_split_stream(self):
+		self.fc8_shape = 100
+		self.fc8 = tf.layers.dense(self.lstm_hidden_state,self.fc8_shape,activation=tf.nn.relu)
+		self.sigmoid_split = tf.layers.dense(self.fc8,1,activation=tf.nn.sigmoid)
+	
+		# Pixel indices, NOT normalized.
+		self.lower_lim = tf.placeholder(tf.float32,shape=(None,1),name='lower_lim')
+		self.upper_lim = tf.placeholder(tf.float32,shape=(None,1),name='upper_lim')
+
+		self.predicted_split = tf.multiply(self.upper_lim-self.lower_lim,self.sigmoid_split)+self.lower_lim
+
+		self.target_split = tf.placeholder(tf.float32,shape=(None,1),name='target_split')
+		self.split_mse = tf.losses.mean_squared_error(self.target_split,self.predicted_split)
+		self.split_return_weight = tf.placeholder(tf.float32,shape=(None,1),name='split_return_weight')
+		self.split_loss = tf.multiply(self.split_mse,self.split_return_weight)
+
+	def logging_ops(self):
+		# Create file writer to write summaries. 		
+		self.tf_writer = tf.summary.FileWriter('train_logging'+'/',self.sess.graph)
+
+		# Create summaries for: Log likelihood, reward weight, and total reward on the full image. 
+		self.rule_loglikelihood_summary = tf.summary.scalar('Rule_LogLikelihood',tf.reduce_mean(self.rule_cross_entropy))
+		self.reward_weight_summary = tf.summary.scalar('Reward_Weight',tf.reduce_mean(self.rule_return_weight))
+
+		# Merge summaries. 
+		self.merged_summaries = tf.summary.merge_all()		
+
+	def training_ops(self):
+
+		self.split_loss_lambda = tf.constant(1.)
+
+		self.total_loss = self.rule_loss+tf.multiply(self.split_loss_lambda,self.split_loss)
+		# self.total_loss = self.split_loss
+
+		# Creating a training operation to minimize the total loss.
+		self.optimizer = tf.train.AdamOptimizer(1e-4)
+		self.train = self.optimizer.minimize(self.total_loss,name='Adam_Optimizer')
+
+		# Writing graph and other summaries in tensorflow.
+		self.writer = tf.summary.FileWriter('training',self.sess.graph)
+		init = tf.global_variables_initializer()
+		self.sess.run(init)
+	
+	def model_load(self, model_file):
+		# DEFINING CUSTOM LOADER:
+		print("RESTORING MODEL FROM:", model_file)
+		reader = tf.train.NewCheckpointReader(model_file)
+		saved_shapes = reader.get_variable_to_shape_map()
+		var_names = sorted([(var.name, var.name.split(':')[0]) for var in tf.global_variables()
+			if var.name.split(':')[0] in saved_shapes])
+		restore_vars = []
+		name2var = dict(zip(map(lambda x:x.name.split(':')[0], tf.global_variables()), tf.global_variables()))
+		with tf.variable_scope('', reuse=True):
+			for var_name, saved_var_name in var_names:
+				curr_var = name2var[saved_var_name]
+				var_shape = curr_var.get_shape().as_list()
+				if var_shape == saved_shapes[saved_var_name]:
+					restore_vars.append(curr_var)
+		saver = tf.train.Saver(max_to_keep=None,var_list=restore_vars)
+		saver.restore(self.sess, model_file)
+
+	def save_model(self, model_index, iteration_number=-1):
+		if not(os.path.isdir("saved_models")):
+			os.mkdir("saved_models")
+
+		self.saver = tf.train.Saver(max_to_keep=None)           
+
+		if not(iteration_number==-1):
+			save_path = self.saver.save(self.sess,'saved_models/model_epoch{0}_iter{1}.ckpt'.format(model_index,iteration_number))
+		else:
+			save_path = self.saver.save(self.sess,'saved_models/model_epoch{0}.ckpt'.format(model_index))
+
+	def create_network(self, sess, pretrained_weight_file=None, to_train=False):
+
+		self.initialize_base_model(sess,to_train=to_train)
+		self.define_LSTM()
+		self.define_rule_stream()
+		self.define_split_stream()
+		self.logging_ops()
+		self.training_ops()
+
+		if pretrained_weight_file:
+			self.model_load(pretrained_weight_file)
