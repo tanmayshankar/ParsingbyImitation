7caf0bc0303ab99d554b0ba35fcff1f9b4a547eb
Ubuntu <ubuntu@ip-172-31-1-139.us-west-2.compute.internal>
2018-06-10 19:11:49 +0000

    update

--- CoRLRealImages/MixedAggreVaTeDPG/DDPGAggrevateParser.py
+++ CoRLRealImages/MixedAggreVaTeDPG/DDPGAggrevateParser.py
@@ -343,13 +343,16 @@ class Parser():
 		for j in reversed(range(len(self.parse_tree))):	
 			if (self.parse_tree[j].backward_index>=0):
 				self.parse_tree[self.parse_tree[j].backward_index].reward += self.parse_tree[j].reward*self.gamma
-
-		for j in range(len(self.parse_tree)):
-			self.parse_tree[j].reward /= (self.parse_tree[j].w*self.parse_tree[j].h)
+		if self.args.train:
+			for j in range(len(self.parse_tree)):
+				self.parse_tree[j].reward *= 10./(self.parse_tree[j].w*self.parse_tree[j].h)
+		else:
+			for j in range(len(self.parse_tree)):
+				self.parse_tree[j].reward /= (self.parse_tree[j].w*self.parse_tree[j].h)
 
 		# # Scaling rewards by constant value - image_size **2 .(so it's at maximum 1).
 		# for j in range(len(self.parse_tree)):
-		#	self.parse_tree[j].reward /= (self.data_loader.image_size**2)
+		#	self.parse_tree[j].reward *= 10./(self.data_loader.image_size**2)
 
 	def backprop(self, iter_num):
 		self.batch_states = npy.zeros((self.batch_size,self.data_loader.image_size,self.data_loader.image_size,self.data_loader.num_channels))
--- CoRLRealImages/MixedAggreVaTeDPG/Meta_TestScript.py
+++ CoRLRealImages/MixedAggreVaTeDPG/Meta_TestScript.py
@@ -8,9 +8,9 @@ for i in range(0,500,5):
 	# command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350BINLABS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --train 0 --gpu 0,1 --depth_terminate 1 --model saved_models/model_epoch{0}.ckpt --suffix model_dt4_{0}".format(i)
 	# command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350BINLABS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --train 0 --gpu 2,3 --depth_terminate 1 --mix 0 --model saved_models/model_epoch{0}.ckpt --suffix model_mix_{0}".format(i)
 
-	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TrainIndices.npy --train 0 --gpu 6,7 --model saved_models/model_epoch{0}.ckpt --suffix model_trainval_{0}".format(i)
+	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TrainIndices.npy --train 0 --reg 1 --gpu 5,4 --model saved_models/model_epoch{0}.ckpt --suffix model_trainval_{0}".format(i)
 	subprocess.call(command.split(),shell=False)
-	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TestIndices.npy --train 0 --gpu 6,7 --model saved_models/model_epoch{0}.ckpt --suffix model_test_{0}".format(i)
+	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TestIndices.npy --train 0 --reg 1 --gpu 5,4 --model saved_models/model_epoch{0}.ckpt --suffix model_test_{0}".format(i)
 	subprocess.call(command.split(),shell=False)
 
 	# FOR RANDOMSQUARES
