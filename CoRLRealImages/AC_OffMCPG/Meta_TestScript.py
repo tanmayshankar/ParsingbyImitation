2c665a75f9976c7ebc5997735da214dc2aae0a3f
tanmayshankar <tanmay.shankar@gmail.com>
2018-08-05 14:10:13 -0400

    Updating test files

--- CoRLRealImages/AC_OffMCPG/Meta_TestScript.py
+++ CoRLRealImages/AC_OffMCPG/Meta_TestScript.py
@@ -10,9 +10,9 @@ for i in range(0,510,5):
 	# command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350BINLABS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --train 0 --gpu 2,3 --depth_terminate 1 --mix 0 --model saved_models/model_epoch{0}.ckpt --suffix model_mix_{0}".format(i)
 
     # TEST AND TRAIN SET
-	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TrainIndices.npy --train 0 --gpu 3,4 --model saved_models/model_epoch{0}.ckpt --suffix model_trainval_{0}".format(i)
+	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TrainIndices.npy --train 0 --gpu 1,0 --plot 0 --model saved_models/model_epoch{0}.ckpt --suffix model_trainval_{0}".format(i)
 	subprocess.call(command.split(),shell=False)
-	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TestIndices.npy --train 0 --gpu 3,4 --model saved_models/model_epoch{0}.ckpt --suffix model_test_{0}".format(i)
+	command = "python ../Meta_RLClass.py --images ../../../ImageSets/NEW350IMS.npy --labels ../../../ImageSets/NEW350BINLABS.npy --indices ../../../ImageSets/TestIndices.npy --train 0 --gpu 2,3 --plot 0 --model saved_models/model_epoch{0}.ckpt --suffix model_test_{0}".format(i)
 	subprocess.call(command.split(),shell=False)
 
 	# # FOR RANDOMSQUARES
